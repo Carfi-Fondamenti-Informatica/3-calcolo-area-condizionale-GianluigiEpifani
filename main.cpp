8e70502b1f3892685e5fc2c2020f951bb7ad95f7
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-04-01 16:05:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+   float a=0, b=0, triangolo=0, quadrato, rettangolo;
+   int opzione=0;
+   cout << "inserire due valori reali e un numero intero\n";
+   cin >> a >> b >> opzione;
+   switch(opzione){
+       case 0:
+           triangolo = a*b/2;
+           cout << triangolo << endl;
+           break;
+       case 1:
+           quadrato = a*a;
+           cout << quadrato << endl;
+           break;
+       case 2:
+           rettangolo = a*b;
+           cout << rettangolo << endl;
+       default:
+           cout << "opzione non valida";
+   }
+    return 0;
 }
